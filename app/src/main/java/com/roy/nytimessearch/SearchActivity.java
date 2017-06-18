a0f289246d36cf5e19c811d3f8ce678c53107ee7
madhurimaroy18 <madhurimaroy18@gmail.com>
2017-06-18 08:56:39 -0700

    Initial commit

---
+++ app/src/main/java/com/roy/nytimessearch/SearchActivity.java
@@ -0,0 +1,64 @@
+package com.roy.nytimessearch;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.GridView;
+import android.widget.Toast;
+
+public class SearchActivity extends AppCompatActivity {
+
+    EditText etQuery;
+    GridView gvResult;
+    Button btnSearch;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        setupViews();
+
+    }
+
+    public void setupViews(){
+        etQuery = (EditText) findViewById(R.id.etQuery);
+        gvResult = (GridView) findViewById(R.id.gvResult);
+        btnSearch = (Button) findViewById(R.id.btnSearch);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_search, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void onArticleSearch(View view) {
+        String query = etQuery.getText().toString();
+        Toast.makeText(this, "Search query is " + query, Toast.LENGTH_LONG).show();
+    }
+}
+
